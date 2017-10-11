5452502334b60c0671946c8aac4c7f2d7e00a403
stephen <syk4367@rit.edu>
2017-10-11 18:44:49 +0000

    Added docs to State

--- State.py
+++ State.py
@@ -1,5 +1,9 @@
 import Pixel
-
+"""
+Class representing the state of each pixel.
+This class contains information of its own terrain, elevation,
+(x,y) coordinate and the coordinates of its goal state (goalX, goalY)
+"""
 class State:
 	def __init__(self, elevation, terrain, x, y, goalX, goalY):
 		self.elevation = elevation
@@ -10,14 +14,17 @@ class State:
 		self.goalY = goalY
 		self.priority = (((goalX - x)**2 + (goalY - y)**2 )**(1/2)) * Pixel.Diag()
 		
+	# for heap purposes
 	def __lt__(other, self):
 		if isinstance(other, self.__class__):
 			return self.x < other.x
 		return NotImplemented
 		
+	# checks if the state is goal
 	def isGoal(self):
 		return self.x == self.goalX and self.y == self.goalY
-		
+	
+	# print function for debugging purposes	
 	def __str__(self):
 		return "this is a state at (" + str(self.x) + ", " + str(self.y) + ")"
 		
